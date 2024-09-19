e78174a56acbc56aa48d5134fcec88d444974f22
longtongster <sacha.van.weeren@gmail.com>
2024-09-19 20:30:04 +0200

    working version with getting data

--- cybulde/process_data.py
+++ cybulde/process_data.py
@@ -1,10 +1,19 @@
 from cybulde.config_schemas.config_schema import Config
 from cybulde.utils.config_utils import get_config
+from utils.data_utils import run_command
 
 
 @get_config(config_path="../configs", config_name="config")
 def process_data(config: Config) -> None:
     print(config)
+    version = "v1"
+    data_local_save_dir = "./data/raw"
+    dvc_remote_repo = "https://github.com/longtongster/project_template_ml.git"
+    dvc_data_folder = "./data/raw"
+    github_user_name = "longtongster"
+    github_access_token = "no_way"
+
+    run_command()
 
 
 if __name__ == "__main__":
--- cybulde/utils/data_utils.py
+++ cybulde/utils/data_utils.py
@@ -1,14 +1,33 @@
+import subprocess
+from shutil import rmtree
+
+def run_shell_command(cmd: str) -> str:
+    return subprocess.run(cmd, text=True, shell=True, check=True, capture_output=True).stdout
+
+
 def get_cmd_to_get_raw_data(
-        version: str,
-        data_local_save_dir: str,
-        dvc_remote_repo_name: str,
-        dvc_data_folder: str,
-        github_user_name: str,
-        github_access_token: str,
+        version: str ="a",
+        data_local_save_dir: str ="./data/",
+        dvc_remote_repo: str ="a",
+        dvc_data_folder: str ="./data/raw/",
+        github_user_name: str="a",
+        github_access_token: str="a",
 ) -> str:
     """Get some data stuff""" 
 
-    command =  f"dvc get {dvc_remote_repo} {dvc_raw_data}"
-    print("piemel")
+    dvc_remote_repo = "git@github.com:longtongster/project_template_ml.git"
+    # we might need to make this
+    # dvc_remote_repo = "<usern>:<acc_tok>@git@github.com:longtongster/project_template_ml.git"
+    command =  f"dvc list {dvc_remote_repo} {dvc_data_folder}"
+    #command =  f"dvc get {dvc_remote_repo} {dvc_data_folder} --rev {version} -o {data_local_save_dir}"
+    print(command)
+    return command
 
-get_cmd_to_get_raw_data()
+def run_command():
+    rmtree("./data/", ignore_errors=True)
+    
+    command = get_cmd_to_get_raw_data()
+    value = run_shell_command(command)
+    print(value)
+
+run_command()
